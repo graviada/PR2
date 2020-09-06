3e8eaac922a71dc3d46163e88f88b03c0594376b
ChalovaI <irish.moscow@gmail.com>
2020-09-06 22:26:05 +0300

    Commit: addemgg

---
+++ src/ru/chalovai/practice1/TestAuthor.java
@@ -0,0 +1,8 @@
+package ru.chalovai.practice1;
+
+public class TestAuthor {
+    public static void main (String []args) {
+        Author author1 = new Author("Igor", "igorloh.ru", 'm');
+        System.out.println(author1);
+    }
+}
